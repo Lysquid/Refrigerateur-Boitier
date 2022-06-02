7f21220aa6aabdc18769439ed434f434ce59018f
Lysquid <romainbenoit21560@gmail.com>
2022-06-02 10:00:46 +0200

    temperature tension

--- src/temperature.cpp
+++ src/temperature.cpp
@@ -11,32 +11,13 @@ void temperatureSetup()
 
 paquetInfos_t temperatureLoop(paquetInfos_t paquetInfos)
 {
-  Serial.print("Temperature  : ");
-
   float anaValue = analogRead(anaPinTemperature);
-  float tension = anaValue * 5.0 / 1023.0;
-  // temperature = (tension-1.25)/0.25;
-  // à la sortie de notre interface, on a Vout(T) = 0.25*T+1.25 d'après la simulation theorique
-  // experimentalement, on calibre notre capteur en ajoutant un gain de 0.95 et un offset de -0.55
-  // après recalibrage pour la nouvelle architecture, nous obtenons les valeurs suivantes :
-  temperature2 = 16.35 * (tension - 3.17);
+  float tension = anaValue * 5.0 / 1024.0;
 
-  if (tension == 5)
-  {
-    Serial.print("> 30");
-  }
-  else if (tension == 0)
-  {
-    Serial.print("< -10");
-  }
-  else
-  {
-    Serial.print(temperature2);
-  }
-  Serial.print("°C (");
+  Serial.print("Temperature  : ");
   Serial.print(tension);
-  Serial.println("V)");
+  Serial.println("V");
 
-  paquetInfos.temperature = temperature2 * 100;
+  paquetInfos.temperature = tension * 100;
   return paquetInfos;
 }
